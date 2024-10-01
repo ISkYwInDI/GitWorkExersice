5ac98c95d2200f287bb2a909f92c18c3cb760003
ISkYwInDI <izimov04@mail.ru>
2024-10-02 01:21:23 +0500

    #

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,57 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        Console.WriteLine("Простой калькулятор");
+        Console.Write("Введите оператор (+, -, *, /, ^ для возведения в степень, √ для квадратного корня): ");
+        char op = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        Console.Write("Введите первое число: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        double result = 0;
+        if (op == '√')
+            result = Math.Sqrt(num1);
+        else
+        {
+            Console.Write("Введите второе число: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка: деление на ноль невозможно!");
+                        return;
+                    }
+                    break;
+                case '^':
+                    result = Math.Pow(num1, num2);
+                    break;
+                default:
+                    Console.WriteLine("Ошибка: неверный оператор!");
+                    return;
+            }
+            Console.WriteLine($"Результат: {num1} {op} {num2} = {result}");
+        }
+
+        if (op == '√')
+        {
+            Console.WriteLine($"Результат: √{num1} = {result}");
+        }
+    }
+}
